4d327b0f89680d52c9c70f22a56c0681c88d3c27
Edgars Miškins <22904432+EmiXikes@users.noreply.github.com>
2021-08-16 15:50:57 +0300

    Added new systems and did some clean up.

--- EpicApp.cs
+++ EpicApp.cs
@@ -1,5 +1,7 @@
 ﻿using Autodesk.Revit.UI;
 using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Revit_EpicRibbon
 {
@@ -22,55 +24,32 @@ namespace Revit_EpicRibbon
 
             // EL
             PushButtonData btn_H2 = new PushButtonData("btn_H2", "H2", dllPath, "Revit_SystemSetter.SetSystem_H2");
-            PushButtonData btn_H201 = new PushButtonData("btn_H201", "H201",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_H201");
-            PushButtonData btn_H202 = new PushButtonData("btn_H202", "H202",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_H202");
-            PushButtonData btn_H203 = new PushButtonData("btn_H203", "H203",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_H203");
+            PushButtonData btn_H201 = new PushButtonData("btn_H201", "H201", dllPath, "Revit_SystemSetter.SetSystem_H201");
+            PushButtonData btn_H202 = new PushButtonData("btn_H202", "H202", dllPath, "Revit_SystemSetter.SetSystem_H202");
+            PushButtonData btn_H203 = new PushButtonData("btn_H203", "H203", dllPath, "Revit_SystemSetter.SetSystem_H203");
+            btn_H2.Image = new BitmapImage(new Uri(@"C:\_x\source\Refs\EpicStyle\IMG\1_16.ico"));
+            btn_H2.LargeImage = new BitmapImage(new Uri(@"C:\_x\source\Refs\EpicStyle\IMG\1_32.ico"));
 
-            PushButtonData btn_H2V1 = new PushButtonData("btn_H2V1", "SAME",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_H2V1");
-            PushButtonData btn_H2V2 = new PushButtonData("btn_H2V2", "NEW",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_H2V2");
-            PushButtonData btn_H2V3 = new PushButtonData("btn_H2V3", "OLD",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_H2V3");
+            //Voids
+            PushButtonData btn_H2V1 = new PushButtonData("btn_H2V1", "SAME", dllPath, "Revit_SystemSetter.SetSystem_H2V1");
+            PushButtonData btn_H2V2 = new PushButtonData("btn_H2V2", "NEW", dllPath, "Revit_SystemSetter.SetSystem_H2V2");
+            PushButtonData btn_H2V3 = new PushButtonData("btn_H2V3", "OLD", dllPath, "Revit_SystemSetter.SetSystem_H2V3");
 
             // EL + ESS
-            PushButtonData btn_HJ201 = new PushButtonData("btn_HJ201", "HJ201",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_HJ201");
+            PushButtonData btn_HJ201 = new PushButtonData("btn_HJ201", "HJ201", dllPath, "Revit_SystemSetter.SetSystem_HJ201");
 
 
             // ESS
-            PushButtonData btn_J2 = new PushButtonData("btn_J2", "J2",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_J2");
-            PushButtonData btn_J201 = new PushButtonData("btn_J201", "J201",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_J201");
+            PushButtonData btn_J2 = new PushButtonData("btn_J2", "J2", dllPath, "Revit_SystemSetter.SetSystem_J2");
+            PushButtonData btn_J201 = new PushButtonData("btn_J201", "J201" ,dllPath, "Revit_SystemSetter.SetSystem_J201");
 
             // Lights
-            PushButtonData btn_H501 = new PushButtonData("btn_H501", "H501",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_H501");
-            PushButtonData btn_H502 = new PushButtonData("btn_H502", "H502",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_H502");
+            PushButtonData btn_H501 = new PushButtonData("btn_H501", "H501" ,dllPath, "Revit_SystemSetter.SetSystem_H501");
+            PushButtonData btn_H502 = new PushButtonData("btn_H502", "H502" ,dllPath, "Revit_SystemSetter.SetSystem_H502");
 
             //Lightning
-            PushButtonData btn_H701 = new PushButtonData("btn_H701", "H701",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_H701");
-            PushButtonData btn_H702 = new PushButtonData("btn_H702", "H702",
-                @"C:\Users\User\source\repos\_Revit\Revit_SystemSetter\bin\Debug\Revit_SystemSetter.dll",
-                "Revit_SystemSetter.SetSystem_H702");
+            PushButtonData btn_H701 = new PushButtonData("btn_H701", "H701" ,dllPath, "Revit_SystemSetter.SetSystem_H701");
+            PushButtonData btn_H702 = new PushButtonData("btn_H702", "H702" ,dllPath, "Revit_SystemSetter.SetSystem_H702");
 
 
 
